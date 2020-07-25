bf20d0fc3803979626f863df5251243577a82afe
jonathan343 <jonathangee09@gmail.com>
2020-07-25 04:36:38 -0500

    Navbar Fix Mobile

--- main.js
+++ main.js
@@ -1,2 +1,13 @@
 // Closes the navigation after use or touch
-$(document).on('click',function(){ $('.collapse').collapse('hide'); })
+
+$(document).ready(function () {
+    $(document).click(
+        function (event) {
+            var target = $(event.target);
+            var _mobileMenuOpen = $(".navbar-collapse").hasClass("show");
+            if (_mobileMenuOpen === true && !target.hasClass("navbar-toggler")) {
+                $("button.navbar-toggler").click();
+            }
+        }
+    );
+});
